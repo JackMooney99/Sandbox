a9679cf9e141af498aef26081b118a9ec4dea68b
jc339958 <jack.mooney@my.jcu.edu.au>
2017-08-18 11:33:26 +1000

    Name printing with Error checking

--- oddName.py
+++ oddName.py
@@ -1,1 +1,29 @@
 '''Jack Mooney'''
+
+def get_name():
+    print("What is your name: ")
+    name = input()
+
+    return name
+
+
+def main():
+    name = get_name()
+    letters = print_every_2nd_letter(name, 2)
+    print("Letters are: {}".format(letters))
+
+
+def print_every_2nd_letter(name, n):
+    s = ''
+    if len(name) != 0:
+        for i in range(1, len(name)):
+            if (i + 1) % n == 0:
+                s += name[i]
+        return s
+
+    else:
+        return "Error. Your name length is 0."
+
+
+if __name__ == "__main__":
+    main()
